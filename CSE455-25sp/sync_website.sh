b2672438d3db24c29d87650f06b720b42386a51e
speedstorm1 <speedstorm123@gmail.com>
2025-04-08 10:42:57 -0700

    add Ayush and Ruta OH Zoom links

--- CSE455-25sp/sync_website.sh
+++ CSE455-25sp/sync_website.sh
@@ -3,4 +3,4 @@
 rsync -avz \
   --exclude .git/ \
   --exclude venv/ \
-   . nkhan51@tricycle.cs.washington.edu:/cse/web/courses/cse455/25sp/.
+   . ayush123@attu.cs.washington.edu:/cse/web/courses/cse455/25sp/.
